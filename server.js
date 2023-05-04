b4fe785279ede2e242c0a3081ffc026ddef69479
unknown <pavanrajesh365@gmail.com>
2023-05-04 15:39:04 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 app.set("viewengine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
